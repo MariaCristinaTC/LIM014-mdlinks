be5d2a92c9001eff1a9b4d1864cd0e1f3a5b3213
MariaCristinaTC <mariacristina.tarazona@outlook.com>
2021-03-24 14:58:26 -0500

    path exists 1st attemp

--- index.js
+++ index.js
@@ -4,14 +4,24 @@ module.exports = () => {
 const fs = require("fs");
 const path = require('path');
 const http = require('http');
+const { Console } = require("console");
 const examplePath = './mdtest/readme.md'; //saving a path as a example
 function validatingAnsolutePath(examplePath) {
     console.log(path.isAbsolute(examplePath))
     return path.isAbsolute(examplePath) === true ? examplePath : path.resolve(examplePath) // the conditional ternary operator
 }
-
-
-
 console.log(path.isAbsolute('/test/demo_path.js')); //true
 console.log(path.isAbsolute('test/demo_path.js')); //false
 console.log(path.isAbsolute('C:\\test\\demo_path.js')); //true
+
+// try {
+//     if (fs.existsSync(path)) {
+//         //file exists
+//     }
+// } catch (err) {
+//     console.error(err)
+// }
+
+function validateIfPathExists(examplePath) {
+    console.log(path)
+}
