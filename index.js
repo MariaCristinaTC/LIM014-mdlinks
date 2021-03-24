bbd3da35c6bafc7d3b7f5fdba57ffb8d24193e89
MariaCristinaTC <mariacristina.tarazona@outlook.com>
2021-03-24 11:03:25 -0500

    example Path and ternary operator for absolute path

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 module.exports = () => {
     // ...
 };
+const fs = require("fs");
+const path = require('path');
+const http = require('http');
+const examplePath = './mdtest/readme.md'; //saving a path as a example
+function validatingAnsolutePath(examplePath) {
+    console.log(path.isAbsolute(examplePath))
+    return path.isAbsolute(examplePath) === true ? examplePath : path.resolve(examplePath) // the conditional ternary operator
+}
+
+
+
+console.log(path.isAbsolute('/test/demo_path.js')); //true
+console.log(path.isAbsolute('test/demo_path.js')); //false
+console.log(path.isAbsolute('C:\\test\\demo_path.js')); //true
